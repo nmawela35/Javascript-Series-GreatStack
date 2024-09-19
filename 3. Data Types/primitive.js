9b980430a08c28cf50f281035dc8e216cfd54ea5
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-19 23:22:33 +0200

    #03 primitive data types intro

---
+++ 3. Data Types/primitive.js
@@ -0,0 +1,31 @@
+//! Data types
+
+//*String
+// let text1 = "Elon"
+// console.log(text1)
+
+//*Number
+// let num =25
+// console.log(num) //output 67
+// console.log(typeof num) 
+
+//*Boolean
+// let washing =true;
+// let running =false;
+// console.log(typeof running)
+
+// let x =5,y=20;
+// console.log(x>y) //false
+
+//*Undefined - variable declared but value not assigned
+// let x;
+// console.log(x)
+
+//* Null -empty/ unknown value;
+let x =null;
+console.log(x)
+console.log(typeof x) //object, known bug in JS, should be null
+
+console.log(null ==undefined) //true, null is undefined in JS
+
+
