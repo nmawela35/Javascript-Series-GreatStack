365ee5f46d382d15e2dbaf59c18e32a669182c56
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-20 11:48:48 +0200

    Data types intro complete

--- 4. Operators/script.js
+++ 4. Operators/script.js
@@ -86,21 +86,53 @@
 // console.log(a) //6 ie. 7-1  updated value
 
 //!Comparison Operators -true/false
-let a =10;
-let b = 20;
+// let a =10;
+// let b = 20;
 
-console.log(a<b) //true
-console.log(a>b) //false
-console.log(a <=b) //true
-console.log(a >=b) //false
+// console.log(a<b) //true
+// console.log(a>b) //false
+// console.log(a <=b) //true
+// console.log(a >=b) //false
 
-console.log(a == b) //false
-console.log(a != b) //true
+// console.log(a == b) //false
+// console.log(a != b) //true
 
-console.log(a === b) //false
-console.log(a !== b) //true
+// console.log(a === b) //false
+// console.log(a !== b) //true
 
 //!Logical Operators
 
+//* && both operands must be true
+// let x = 5;
+// let y = 10;
+
+// console.log(x > 0 && y > 0) //true
+// console.log(x > 0 && y < 0) //false
+// console.log(x < 0 && y > 0) //false
+// console.log(x < 0 && y < 0) //false
+
+//* || - one of the operands is true
+// let a = 5;
+// let b = 10;
+
+// console.log(a > 0 || b > 0) //true
+// console.log(a > 0 || b < 0) //true
+// console.log(a < 0 || b > 0) //true
+// console.log(a < 0 || b < 0) //false
+
+//* ! Logical NOT - convert operator to boolean and returns flipped value
+let YES = true;
+let NO =false;
+let x = 5;
+let emptyString =""
+let nothing =null
+let notAssigned = undefined;
+
+console.log(!YES); //false
+console.log(!NO)    //true
+console.log(!x) //false
+console.log(!emptyString) //true
+console.log(!nothing) //true
+console.log(!notAssigned) //true
 
 //!String Operators
