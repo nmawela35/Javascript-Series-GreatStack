107b8e43d89b6d389a9c9088be8b0112c6d4bc39
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-20 11:05:57 +0200

    comparison operators confusing

---
+++ 4. Operators/script.js
@@ -0,0 +1,106 @@
+
+//!Arithmetic Operators - perform mathematical operations
+
+//*addition
+// let sum = 5 + 3;
+// console.log({sum})
+
+//*subtraction
+// let subtraction = 5 - 3;
+// console.log({subtraction})
+
+//*multiplication
+// let multiplication = 5 * 3;
+// console.log({multiplication})
+
+//*division
+// let division = (53 / 3);
+// console.log({division})
+
+//*modulus - 
+// let remainder = 15 %4
+// console.log({remainder})
+
+//*exponentiation
+// let exponentiation = 3**3
+// console.log({exponentiation})
+
+
+//!Assignment Operators
+
+//* addition assignment
+// var x =5;
+// x+=7
+// console.log(x); //x+7
+
+//*subtraction assignment
+// let x = 10;
+// x -= 8
+// console.log(x)
+
+//*multiplication assignment
+// let x = 10;
+// x *= 8
+// console.log(x)
+
+//*division assignment
+// let x = 100;
+// x /= 2
+// console.log(x)
+
+//*modulus assignment
+// let x =10;
+// x %= 3;
+// console.log(x);
+
+//*multiplication assignment
+// let x = 5;
+// x **=3;
+// console.log(x);
+
+//*division assignment
+// let x = 15;
+// x /=3;
+// console.log(x);
+
+//!Increment / decrement Operator
+
+//*prefix increment
+// let a = 10;
+// console.log(++a) //11 ie. 10+1 increased before it is used
+// console.log(a) //11 
+
+//*prefix decrement
+// let a = 20;
+// console.log(--a) //19  decreased before it is used
+// console.log(a) //19 ie. a-1,
+
+//* postfix increment
+// a = 15;
+// console.log(a++) //15, value used before incremented
+// console.log(a) //16 ie. 15+1 updated value
+
+//* post-fixed decrement
+// a = 7;
+// console.log(a--) //7,  value used before incremented
+// console.log(a) //6 ie. 7-1  updated value
+
+//!Comparison Operators -true/false
+let a =10;
+let b = 20;
+
+console.log(a<b) //true
+console.log(a>b) //false
+console.log(a <=b) //true
+console.log(a >=b) //false
+
+console.log(a == b) //false
+console.log(a != b) //true
+
+console.log(a === b) //false
+console.log(a !== b) //true
+
+//!Logical Operators
+
+
+//!String Operators
